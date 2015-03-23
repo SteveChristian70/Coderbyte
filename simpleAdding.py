eb7e30a895c3c65587cbda73d7b861760ace4a76
Steve Christian <Stevechristian70@gmail.com>
2015-03-22 19:56:39 -0700

    Update simpleAdding.py

--- simpleAdding.py
+++ simpleAdding.py
@@ -1,8 +1,10 @@
-'''Have the function SimpleAdding(num) add up all the numbers from 1 to num.
+'''
+Have the function SimpleAdding(num) add up all the numbers from 1 to num.
 For the test cases, the parameter num will be any number from 1 to 1000. 
 
 Use the Parameter Testing feature in the box below
-to test your code with different arguments.'''
+to test your code with different arguments.
+'''
 
 def SimpleAdding(num): 
 
