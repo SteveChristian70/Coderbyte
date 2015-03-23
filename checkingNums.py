4d03dfb392b142b0e7d91c91642d4f60d035e220
Steve Christian <Stevechristian70@gmail.com>
2015-03-22 20:00:15 -0700

    Update checkingNums.py

--- checkingNums.py
+++ checkingNums.py
@@ -1,10 +1,12 @@
-'''Have the function CheckNums(num1,num2) take both parameters being
+'''
+Have the function CheckNums(num1,num2) take both parameters being
  passed and return the string true if num2 is greater than num1, 
  otherwise return the string false. If the parameter values are
   equal to each other then return the string -1. 
 
 Use the Parameter Testing feature in the box below to
- test your code with different arguments.'''
+ test your code with different arguments.
+'''
 
 
 def CheckNums(num1,num2): 
@@ -16,6 +18,5 @@ def CheckNums(num1,num2):
   else:
     return "-1"
     
-# keep this function call here  
-# to see how to enter arguments in Python scroll down
+
 print CheckNums(raw_input())
