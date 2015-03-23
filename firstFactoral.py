962679bba602066fed33e67ba91d39f61195e7ec
Steve <SteveChristian70@gmail.com>
2015-03-22 20:59:46 -0700

    Upload

--- firstFactoral.py
+++ firstFactoral.py
@@ -1,8 +1,10 @@
-'''Have the function FirstReverse(str) take the str
+'''
+Have the function FirstReverse(str) take the str
  parameter being passed and return the string in reversed order. 
 
 Use the Parameter Testing feature in the box below to 
-test your code with different arguments.'''
+test your code with different arguments.
+'''
 
 
 
--- vowelCounter.py
+++ vowelCounter.py
@@ -1,9 +1,11 @@
-'''Have the function VowelCount(str) take the str string parameter 
+'''
+Have the function VowelCount(str) take the str string parameter 
 being passed and return the number of vowels the string contains 
 (ie. "All cows eat grass" would return 5). Do not count y as a vowel for this challenge. 
 
 Use the Parameter Testing feature in the box below to test
- your code with different arguments.'''
+ your code with different arguments.
+'''
 
 
 def VowelCount(str): 
