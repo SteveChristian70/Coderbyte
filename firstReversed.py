2e8c606aa1ff7d10a43e640ea07e7d87f2de8618
Steve <SteveChristian70@gmail.com>
2015-03-23 21:03:00 -0700

    Initial Upload

---
+++ firstReversed.py
@@ -0,0 +1,9 @@
+'''
+Have the function FirstReverse(str) take the str parameter 
+being passed and return the string in reversed order. 
+'''
+
+def FirstReverse(str): 
+  return (str)[::-1]
+  
+print FirstReverse(raw_input())
